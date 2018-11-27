e3fd81aa7bbf30541b35dc929177ca5bf24c84dc
megalon <megalon@users.noreply.github.com>
2018-11-26 20:14:50 -0800

    Add project files.

---
+++ Beat Saber Menu Tweaks/Plugin.cs
@@ -0,0 +1,47 @@
+﻿using IllusionPlugin;
+using UnityEngine.SceneManagement;
+
+namespace Beat_Saber_Menu_Tweaks
+{
+    public class Plugin : IPlugin
+    {
+        public string Name => "Beat Saber Menu Tweaks";
+        public string Version => "0.0.1";
+        public void OnApplicationStart()
+        {
+            SceneManager.activeSceneChanged += SceneManagerOnActiveSceneChanged;
+            SceneManager.sceneLoaded += SceneManager_sceneLoaded;
+        }
+
+        private void SceneManagerOnActiveSceneChanged(Scene arg0, Scene arg1)
+        {
+        }
+
+        private void SceneManager_sceneLoaded(Scene arg0, LoadSceneMode arg1)
+        {
+        }
+
+        public void OnApplicationQuit()
+        {
+            SceneManager.activeSceneChanged -= SceneManagerOnActiveSceneChanged;
+            SceneManager.sceneLoaded -= SceneManager_sceneLoaded;
+        }
+
+        public void OnLevelWasLoaded(int level)
+        {
+
+        }
+
+        public void OnLevelWasInitialized(int level)
+        {
+        }
+
+        public void OnUpdate()
+        {
+        }
+
+        public void OnFixedUpdate()
+        {
+        }
+    }
+}
